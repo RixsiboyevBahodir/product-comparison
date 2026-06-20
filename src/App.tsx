3ab05483bc17058108a4785dd8733fd1eab0977f
RixsiboyevBahodir <rixsiboyevbahodir093@gmail.com>
2026-06-20 12:57:48 +0500

    Various bug fixes and style fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { products } from "./data/products";
 export default function App() {
   return (
     <div className="lg:max-w-280 m-auto p-4">
-      <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2  gap-5 my-4">
+      <div className="grid lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2  gap-5 mt-5 mb-15">
         {products.map((product) => (
           <ProductCard key={product.id} {...product} />
         ))}
